e2b74e3eb91de8504ba4aa97d039121ef31b69fe
huahang <hliu@me.com>
2022-06-06 14:48:55 +0800

    build: Fix build for macOS
    Signed-off-by: huahang <hliu@me.com>

--- tools/cmake_bootstrap.sh
+++ tools/cmake_bootstrap.sh
@@ -2,10 +2,20 @@
 
 mkdir -p cmake-build
 
+OS=$(uname -s)
+
+if [[ "$OS" =~ ^Darwin ]]; then
+  LIBCXX=libc++
+fi
+
+if [[ "$OS" =~ ^Linux ]]; then
+  LIBCXX=libstdc++11
+fi
+
 conan install .                          \
   --install-folder cmake-build           \
   --build                                \
-  --settings compiler.libcxx=libstdc++11
+  --settings compiler.libcxx=$LIBCXX
 
 cmake                                                      \
   -B cmake-build                                           \
