884edaf6fd099bb5661054ec6e7f1656d7707218
hans <hans@dkmt.io>
2022-06-19 22:41:12 +0800

    chore: Fix copyright headers
    Signed-off-by: hans <hans@dkmt.io>

--- dk_pull/types/source.cc
+++ dk_pull/types/source.cc
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2022 Hans Liu
+ * Copyright (C) 2022 Huahang Liu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
