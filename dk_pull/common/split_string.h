adb6b3bfd3ea4d0ff87ad2d79b9f8081dd6712c7
huahang <hliu@me.com>
2022-07-12 00:00:28 +0800

    feat: Add SplitString()
    Signed-off-by: huahang <hliu@me.com>

---
+++ dk_pull/common/split_string.h
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (C) 2022 Huahang Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace dk_pull {
+namespace common {
+
+void SplitString(std::vector<std::string>* result, const std::string& full,
+                 char delim);
+
+void SplitString(std::vector<std::string_view>* result,
+                 const std::string_view& full, char delim);
+
+}  // namespace common
+}  // namespace dk_pull
